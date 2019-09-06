7d5642b3afd2b9e464df27204471b97a187fb231
Rajat0023 <rajatsingh2237@gmail.com>
2019-09-06 18:46:20 +0530

    Re-used same header component as of Login Page

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -3,8 +3,11 @@ import React,{Component} from 'react';
 class Profile extends Component {
     render() {
         return (
-
-    <div></div>
+            <div>
+            <header className="app-header">
+                <span className="header-logo" onClick={this.headerlogoClickHandler}>Image Viewer </span>
+                </header>
+                </div>
         )
     }
 }
